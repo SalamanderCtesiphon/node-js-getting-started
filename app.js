2cf17916baffb00f4a1356dc795b4dcba3472998
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-22 14:01:49 -0500

    something is wrong

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-require('dotenv').config();
-console.log(process.env);
-
-process.env.USER_ID; // "239482"
-process.env.USER_KEY; // "foobar"
-process.env.NODE_ENV; // "development"
-
-console.log(process.env.USER_ID);
-console.log(process.env.USER_KEY);
-console.log(process.env.NODE_ENV);
+import { URL } from 'node:url';
+console.log(URL === globalThis.URL); // Prints 'true'.;
