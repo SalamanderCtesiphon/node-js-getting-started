91d6d873032d8cca541863aecbd4c0f571dfee95
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-22 12:33:35 -0500

    created an html file to test code. also created .env file to test concept. and installed dotenv

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+require('dotenv').config();
+
+process.env.USER_ID; // "239482"
+process.env.USER_KEY; // "foobar"
+process.env.NODE_ENV; // "development"
+
+console.log(process.env.USER_ID);
+console.log(process.env.USER_KEY);
+console.log(process.env.NODE_ENV);
